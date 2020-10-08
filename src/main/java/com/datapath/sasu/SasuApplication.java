7af2ffd06b9c89987233628ca10cad84ce739bdb
eduard.david <eduard.david9@gmail.com>
2020-10-08 14:39:48 +0300

    Init skeleton

---
+++ src/main/java/com/datapath/sasu/SasuApplication.java
@@ -0,0 +1,13 @@
+package com.datapath.sasu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SasuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SasuApplication.class, args);
+    }
+
+}
